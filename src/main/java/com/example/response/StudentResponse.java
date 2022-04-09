749322f9444a6f9ce418d10efd916178c9d00d6f
Romit Sutariya <romitsutariya@gamil.com>
2022-04-09 15:48:45 +0530

    Initial commit for the spring boot rest api

---
+++ src/main/java/com/example/response/StudentResponse.java
@@ -0,0 +1,36 @@
+package com.example.response;
+
+import com.example.entity.Student;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class StudentResponse {
+
+	private long id;
+
+	@JsonProperty("first_name")
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+	
+	private String street;
+
+	private String city;
+	
+	public StudentResponse (Student student) {
+		this.id = student.getId();
+		this.firstName = student.getFirstName();
+		this.lastName = student.getLastName();
+		this.email = student.getEmail();
+		
+		this.street = student.getAddress().getStreet();
+		this.city = student.getAddress().getCity();
+	}
+
+}
---
+++ src/main/java/com/example/service/StudentService.java
@@ -0,0 +1,99 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Address;
+import com.example.entity.Student;
+import com.example.repository.AddressRepository;
+import com.example.repository.StudentRepository;
+import com.example.request.CreateStudentRequest;
+import com.example.request.InQueryRequest;
+import com.example.request.UpdateStudentRequest;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@Autowired
+	AddressRepository addressRepository;
+	
+	public List<Student> getAllStudents () {
+		return studentRepository.findAll();
+	}
+	
+	public Student createStudent (CreateStudentRequest createStudentRequest) {
+		Student student = new Student(createStudentRequest);
+		
+		Address address = new Address();
+		address.setStreet(createStudentRequest.getStreet());
+		address.setCity(createStudentRequest.getCity());
+		
+		address = addressRepository.save(address);
+		
+		student.setAddress(address);
+		student = studentRepository.save(student);
+		return student;
+	}
+	
+	public Student updateStudent (UpdateStudentRequest updateStudentRequest) {
+		Student student = studentRepository.findById(updateStudentRequest.getId()).get();
+		
+		if (updateStudentRequest.getFirstName() != null && 
+				!updateStudentRequest.getFirstName().isEmpty()) {
+			student.setFirstName(updateStudentRequest.getFirstName());
+		}
+		
+		student = studentRepository.save(student);
+		return student;
+	}
+	
+	public String deleteStudent (long id) {
+		studentRepository.deleteById(id);
+		return "Student has been deleted successfully";
+	}
+	
+	public List<Student> getByFirstName (String firstName) {
+		return studentRepository.findByFirstName(firstName);
+	}
+	
+	public Student getByFirstNameAndLastName (String firstName, String lastName) {
+		//return studentRepository.findByLastNameAndFirstName(lastName, firstName);
+		return studentRepository.getByLastNameAndFirstName(lastName, firstName);
+	}
+	
+	public List<Student> getByFirstNameOrLastName (String firstName, String lastName) {
+		return studentRepository.findByFirstNameOrLastName(firstName, lastName);
+	}
+	
+	public List<Student> getByFirstNameIn (InQueryRequest inQueryRequest) {
+		return studentRepository.findByFirstNameIn(inQueryRequest.getFirstNames());
+	}
+	
+	public List<Student> getAllStudentsWithPagination (int pageNo, int pageSize) {
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+		
+		return studentRepository.findAll(pageable).getContent();
+	}
+	
+	public List<Student> getAllStudentsWithSorting () {
+		Sort sort = Sort.by(Sort.Direction.ASC, "firstName", "lastName", "email");
+		
+		return studentRepository.findAll(sort);
+	}
+	
+	public Integer updateStudentWithJpql (Long id, String firstName) {
+		return studentRepository.updateFirstName(id, firstName);
+	}
+	
+	public Integer deleteStudent (String firstName) {
+		return studentRepository.deleteByFirstName(firstName);
+	}
+}
